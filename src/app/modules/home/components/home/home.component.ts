9196578f2aa74c6cccdc91835928afd4e5d83771
Avsar_Vora <avsar@digitaltrons.com>
2021-09-12 22:11:23 +0530

    Hack-1 #Made card component reusable

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Idea } from 'src/app/shared/models/Idea.model';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
         backgroundColor: '#FFF'
       })),
       state('open', style({
-        width: '400px',
+        width: '450px',
         backgroundColor: '#F7F7F7'
       })),
       transition('open <=> close', [
@@ -26,6 +27,18 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   isExpanded = false;
+  cardObj = {
+    title: "The Magnificent Stag",
+    imgUrl: "../../../../../assets/images/7747.jpg",
+    alt: "Image",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque atque eius molestias doloremque
+    amet, porro inventore temporibus veritatis enim excepturi est sapiente recusandae minima quia
+    nihil cumque quasi, quibusdam sequi.`,
+    createdBy: "Avsar Vora",
+    createdOn: "25th Sep, 2021",
+    votesCount: 125,
+    tags: []
+  }
 
   ngOnInit(): void {
   
